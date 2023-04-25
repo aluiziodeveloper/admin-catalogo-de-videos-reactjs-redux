7a67e676df2c65a5e7ca420772645d7d7a4a5e8e
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-04-25 00:08:49 -0300

    adding utils

---
+++ src/utils/buildQueryString.ts
@@ -0,0 +1,14 @@
+export function buildQueryString<T extends Record<string, any>>(
+  params: T,
+): string {
+  const query = new URLSearchParams()
+
+  for (const key in params) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (params.hasOwnProperty(key)) {
+      query.append(key, params[key].toString())
+    }
+  }
+
+  return query.toString()
+}
